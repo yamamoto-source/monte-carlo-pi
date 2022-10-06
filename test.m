80a78bc45f667e91a26d6c80ff0270b38450d243
yamamoto-source <114160021+yamamoto-source@users.noreply.github.com>
2022-10-06 22:28:19 +0900

    Update test.m

--- test.m
+++ test.m
@@ -1,16 +1,18 @@
-N = 10000;
-count = 0;
+N = 10000; 　% 試行回数
+count = 0;　 % 円の中にある点の数
 
+% 半径１の円の1/4を描く
 t = linspace(0,2*pi,100);
 figure
 plot(sin(t),cos(t))
 axis square 
-
 xlim([0,1])
 ylim([0,1])
 
+% 現在の座標軸のプロットを保持する
 hold on
 
+% 0.00001秒ごとに乱数を発生させ座標軸にプロット
 for i = 1:N
     a = rand;
     b = rand;
